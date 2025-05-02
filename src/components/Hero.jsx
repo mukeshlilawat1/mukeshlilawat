c7b6ace6f9f849f914259fc58c1aea1ddd7c1c68
mukeshlilawat <mukeshkumarlilawat1@gmail.com>
2025-05-02 08:36:22 +0530

    update

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -17,20 +17,20 @@ const Hero = () => {
                                 className='img-cover' />
                         </figure>
 
-                        <div className=''>
-                            <span className=''>
-                                <span className=''></span >
+                        <div className='flex items-center gap-1.5  text-zinc-400 text-sm tracking-wide'>
+                            <span className='relative w-2 h-2 rounded-full bg-emerald-400'>
+                                <span className='absolute inset-0 rounded-full bg-emerald-400 animate-ping'></span >
                             </span>
                             Available for work
 
                         </div>
                     </div>
 
-                    <h2 className=''>
+                    <h2 className='headline-1 max-w-[15ch] sm:max-w-[20ch] lg:max-w-[15ch] mt-5 mb-8 lg:mb-10'>
                         Building Scalable Modern Websites for the Future
                     </h2>
 
-                    <div className=''>
+                    <div className='flex items-center gap-3'>
                         ButtonPrimary
 
                         ButtonOutline
@@ -38,8 +38,8 @@ const Hero = () => {
                     </div>
                 </div>
 
-                <div className=''>
-                    <figure className=''>
+                <div className='hidden lg:block'>
+                    <figure className='w-full max-w-[480px] ml-auto bg-gradient-to-t from-purple-400 via-25% via-orange-400/40 to-65% rounded-[60px] overflow-hidden '>
                         <img src='./src/assets/bannner1.png' width={656} height={800} alt='Mukesh Lilawat' className='w-full ' />
                     </figure>
                 </div>
