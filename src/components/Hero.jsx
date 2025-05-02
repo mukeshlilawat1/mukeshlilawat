4c7c6697d52bd03c9903a5b16d9bc50954d11172
mukeshlilawat <mukeshkumarlilawat1@gmail.com>
2025-05-02 07:49:04 +0530

    update

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,13 +3,13 @@ import React from 'react'
 const Hero = () => {
     return (
         <section
-            id='home' className=''>
+            id='home' className='pt-28 lg:pt-36'>
 
-            <div className=''>
+            <div className='container items-center lg:grid lg:grid-cols-2  lg:gap-10'>
 
                 <div>
-                    <div className=''>
-                        <figure className=''>
+                    <div className='flex items-center gap-3'>
+                        <figure className='img-box w-9 h-9 rounded-lg'>
                             <img src='src/assets/images.jpeg'
                                 width={40}
                                 height={40}
@@ -21,8 +21,27 @@ const Hero = () => {
                             <span className=''>
                                 <span className=''></span >
                             </span>
+                            Available for work
+
                         </div>
                     </div>
+
+                    <h2 className=''>
+                        Building Scalable Modern Websites for the Future
+                    </h2>
+
+                    <div className=''>
+                        ButtonPrimary
+
+                        ButtonOutline
+
+                    </div>
+                </div>
+
+                <div className=''>
+                    <figure className=''>
+                        <img src='./src/assets/bannner1.png' width={656} height={800} alt='Mukesh Lilawat' className='w-full ' />
+                    </figure>
                 </div>
 
             </div>
